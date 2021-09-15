d8ffe7dcb6338fee0525c984453404bad966c8c1
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-09-15 19:27:55 +0200

    09 add comment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // Attention for launch this requested switched version Node js to 16.9.1
+// Use nvm ls , nvm use node-version
 // For launch this server API in directory amazona set --> npm start (launch express server)
 // For automatic refresh server i install nodemon and in file package.json written script for start it
 
