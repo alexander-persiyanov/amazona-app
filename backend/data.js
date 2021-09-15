76b64b9710e5758829bfff8d14fc60ae5f149e8b
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-09-15 19:25:19 +0200

    09 create Node Js server

---
+++ backend/data.js
@@ -0,0 +1,85 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike slim shirt',
+            category:'shirt',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:4,
+            brand:'Nike',
+            rating:3.5,
+            numReviews:10,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'2',
+            name:'Adidas slim shirt',
+            category:'shirt',
+            image:'/images/p2.jpg',
+            price:100,
+            countInStock:0,
+            brand:'Nike',
+            rating:2.0,
+            numReviews:10,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'3',
+            name:'Lacoste free shirt',
+            category:'shirt',
+            image:'/images/p3.jpg',
+            price:150,
+            countInStock:20,
+            brand:'Lacoste',
+            rating:5,
+            numReviews:50,
+            description:'best quality product',
+            
+        },
+        {
+            _id:'4',
+            name:'Nike slim pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:90,
+            countInStock:3,
+            brand:'Nike',
+            rating:4.4,
+            numReviews:15,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'5',
+            name:'Puma slim pant',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:60,
+            countInStock:10,
+            brand:'Puma',
+            rating:4.7,
+            numReviews:20,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'6',
+            name:'Adidas fit pant',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:99,
+            countInStock:12,
+            brand:'Adidas',
+            rating:4.9,
+            numReviews:24,
+            description:'high quality product',
+            
+        },
+
+    ],
+}
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// Attention for launch this requested switched version Node js to 16.9.1
+// For launch this server API in directory amazona set --> npm start (launch express server)
+// For automatic refresh server i install nodemon and in file package.json written script for start it
+
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
