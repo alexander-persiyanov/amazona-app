2946f16002375803a070f6fbb6b796ec0f7032d0
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-09-15 11:47:04 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike slim shirt',
+            category:'shirt',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'2',
+            name:'Adidas slim shirt',
+            category:'shirt',
+            image:'/images/p2.jpg',
+            price:100,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:10,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'3',
+            name:'Lacoste free shirt',
+            category:'shirt',
+            image:'/images/p3.jpg',
+            price:150,
+            brand:'Lacoste',
+            rating:5,
+            numReviews:50,
+            description:'best quality product',
+            
+        },
+        {
+            _id:'4',
+            name:'Nike slim pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:90,
+            brand:'Nike',
+            rating:4.4,
+            numReviews:15,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'5',
+            name:'Puma slim pant',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            price:60,
+            brand:'Puma',
+            rating:4.7,
+            numReviews:20,
+            description:'high quality product',
+            
+        },
+        {
+            _id:'6',
+            name:'Adidas fit pant',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:99,
+            brand:'Adidas',
+            rating:4.9,
+            numReviews:24,
+            description:'high quality product',
+            
+        },
+
+    ],
+}
+
+export default data;
