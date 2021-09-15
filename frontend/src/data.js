acb89ea2af6b69795e36ae2e8886d7b749adbe47
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-09-15 14:28:21 +0200

    07 create components Rating+Product

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -7,7 +7,7 @@ const data = {
             image:'/images/p1.jpg',
             price:120,
             brand:'Nike',
-            rating:4.5,
+            rating:3.5,
             numReviews:10,
             description:'high quality product',
             
@@ -19,7 +19,7 @@ const data = {
             image:'/images/p2.jpg',
             price:100,
             brand:'Nike',
-            rating:4.0,
+            rating:2.0,
             numReviews:10,
             description:'high quality product',
             
